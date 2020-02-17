09b027cbf8eb4a31dd058e6de375e5fabb314fb2
Abdelaty <abdelaty.mohammedmagdi@gmail.com>
2020-02-17 21:32:56 +0200

    Add MD5 Encryption method
    Signed-off-by: abdelaty <abdelaty.mohammedmagdi@gmail.com>

--- app/src/main/java/com/example/marvelheroes/MainActivity.kt
+++ app/src/main/java/com/example/marvelheroes/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.marvelheroes
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.marvelheroes.ui.main.MainFragment
+import java.math.BigInteger
+import java.security.MessageDigest
 
 class MainActivity : AppCompatActivity() {
 //public key
@@ -18,6 +20,13 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+        val input = "15cfb621f5407c5143a916c86bb0b674596246a62ebd094990a30b40e97ab5a73fbf86af8"
+        val md5 = input.md5()
+        println("computed md5 value is $md5")
     }
 
+    fun String.md5(): String {
+        val md = MessageDigest.getInstance("MD5")
+        return BigInteger(1, md.digest(toByteArray())).toString(16).padStart(32, '0')
+    }
 }
